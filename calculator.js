e61892274bc3478bd0a7e07c508476a8116e340d
Nicole I Stein <ns60549@srcs.loc>
2018-04-23 10:59:56 -0700

    work from home transfered because it wasnt working on my computer

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 var currentInput = "0"
 var memory = "0"
+
 // Helper function for displaying the current input
 function displayCurrentInput() {
     document.getElementById('screen').value = currentInput;
 }
+
+
