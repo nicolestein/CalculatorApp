b93c7965eb6c09216a38654d0b308097c71c55ca
Nicole I Stein <ns60549@srcs.loc>
2018-04-23 11:32:12 -0700

    wok on js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 var currentInput = "0"
 var memory = "0"
 
-// Helper function for displaying the current input
+// Displaying the current input
 function displayCurrentInput() {
     document.getElementById('screen').value = currentInput;
+}
+
+// Add a digit to the current digit
+function addDig(dig) {
+}
+
+
+// Add a decimal to the current input
+function addDecimal() {
+
 }
 
 
