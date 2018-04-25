6fe9ae7e05c1ba4eea57d44a4bb4449a094d412b
Nicole I Stein <ns60549@srcs.loc>
2018-04-25 10:47:31 -0700

    Work from home part 2

--- calculator.js
+++ calculator.js
@@ -50,3 +50,52 @@ function allClear() {
 }
 
 //square of input
+
+// Change the sign of the current input
+function changeSign() {
+    currentInput = currentInput * (-1);
+    displayCurrentInput();
+}
+
+// Clear the current input back to 0
+function clears() {
+    currentInput = "0";
+    displayCurrentInput();
+}
+
+// Change the current input to a percentage
+function percentage() {
+ currentInput = currentInput/100;
+displayCurrentInput();
+}
+
+// Calculate the factorial of the current input
+function factorial() {
+var input = currentInput;
+var x = input;
+for (i = input-1; i > 1; i--) {
+    x = x*i;
+}
+currentInput = x;
+console.log(currentInput);
+displayCurrentInput();
+}
+
+// Calculate the square of the current input
+function square() {
+ currentInput = Math.pow(currentInput,2);
+    displayCurrentInput();
+}
+
+// Calculate the square root of the current input
+function squareRoot() {
+   currentInput = Math.sqrt(currentInput);
+    displayCurrentInput();
+}
+
+// Calculate the inverse of the current input
+function inverse() {
+currentInput = 1/currentInput;
+displayCurrentInput();
+}
+
